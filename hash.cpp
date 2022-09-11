83804644c4be2202a9f8288b41779eacdb6ad67b
alizameller <alizameller@gmail.com>
2022-09-11 18:44:25 -0400

    first commit

---
+++ hash.cpp
@@ -0,0 +1,50 @@
+#include "hash.h"
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+// find position
+int hashTable::findPos(const string &key){
+    // somehow find position of key
+}
+
+// check if hashTable contains key
+bool hashTable::contains(const string &key){
+    if (findPos(&key) == -1) { // key was not found (i.e. not in table)
+        return false;
+    } else {
+        int pos = findPos(&key);
+        return true;
+    }
+}
+
+// function to get the next prime value for re-hashing
+unsigned int hashTable::getPrime(int size){
+}
+
+// Hash function to calculate hash for a value
+int hashTable::hash(const string &key){
+}
+
+bool hashTable::rehash(){
+    // use getPrime and make new hashTable with size of new prime
+}
+
+// inserting into hashTable
+int hashTable::insert(const string &key, void *pv){
+    if (contains(&key) == false) { //if slot is empty
+        hashTable::hashItem.isOccupied = false;
+        // check if lazily deleted?
+        // check if size >> ___ and if so, call re-hash
+        // insert using key, pv and pushBack function?
+    } else { //if slot is full
+        hashTable::hashItem.isOccupied = true;
+        // linear probing to find next slot
+    }
+}
+
+// instance of hashTable
+hashTable::hashTable(int size){
+}
