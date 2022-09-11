751d48a05ea5553529c8fda9bd4d8d2804b720f6
alizameller <alizameller@gmail.com>
2022-09-11 18:44:55 -0400

    first commit

---
+++ spellcheck.cpp
@@ -0,0 +1,29 @@
+#include "hash.h"
+#include "hash.cpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* Prompts: 
+Enter name of dictionary: 
+Total time (in seconds) to load dictionary: 
+
+Enter name of input file: 
+
+Enter name of output file: 
+Total time (in seconds) to check document: 
+*/
+
+// traverse dictionary file one word at a time
+// insert each word into hash table (dictionary)
+
+// traverse input file one word at a time
+// for each word, check if hash table contains word
+//      if yes, print message in output file
+//      if not, print message in output file
+// go to next word
+
+int main() {
+
+}
