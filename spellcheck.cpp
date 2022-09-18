6176a06228d2e12a4824824ecc3b3f6127cf91d0
alizameller <alizameller@gmail.com>
2022-09-18 18:13:22 -0400

    creating dictionary in the works

--- spellcheck.cpp
+++ spellcheck.cpp
@@ -1,6 +1,6 @@
 #include "hash.h"
-#include "hash.cpp"
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -23,6 +23,15 @@ Total time (in seconds) to check document:
 //      if yes, print message in output file
 //      if not, print message in output file
 // go to next word
+hashTable read (ifstream dictFile) {
+    string word;
+    hashTable dictionary; 
+    while (getline(dictFile, word)) { 
+        dictionary.insert(word); 
+    }
+    return dictionary; 
+}
+
 
 int main() {
     ifstream dictFile; //read Dictionary
@@ -35,9 +44,14 @@ int main() {
 
     cout << "Enter name of dictionary file: ";
     cin >> dictFileName;
-    infile.open(dictFileName);
-    // time display
-    
+    dictFile.open(dictFileName);
+    clock_t read1 = clock();
+    read(ifstream dictFile); 
+    clock_t read2 = clock();
+    double readTimeDiff = ((double) (read2 - read1)) / CLOCKS_PER_SEC;
+
+    cout << "Total time (in seconds) to load dictionary: " << readTimeDiff << "\n";
+
     cout << "Enter name of input file: ";
     cin >> inputFileName;
     inFile.open(inputFileName);
@@ -45,5 +59,10 @@ int main() {
     cout << "Enter name of output file: ";
     cin >> outputFileName;
     outFile.open(outputFileName);
-    // time display
+    clock_t check1 = clock();
+    // check dictionary
+    clock_t check2 = clock();
+    double checkTimeDiff = ((double) (check2 - check1)) / CLOCKS_PER_SEC;
+
+    cout << "Total time (in seconds) to check document: " << checkTimeDiff << "\n";
 }
